d7da4ead67c598fed48ca28a725f8853d272def7
Adarsh108-tech <adarsh16221@gmail.com>
2025-07-28 11:38:18 +0530

    added the pledge section 2

--- src/app/pages/pledge/page.js
+++ src/app/pages/pledge/page.js
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Navbar from "@/components/navbar";
 import Sidebar from "@/components/pledgeSidebar";
 import habits from "./habit.json";
 import { FiGlobe, FiFeather } from "react-icons/fi";
